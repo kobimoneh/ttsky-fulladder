978d987db8a93b12ac175206bab69c162042b3cb
kobimoneh <kobimoneh@gmail.com>
2025-07-15 11:15:01 +0300

    first full adder attempts

--- src/project.v
+++ src/project.v
@@ -17,11 +17,24 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[7:5] = 3'b0;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
-
+  wire w;
+  assign w = 0;
+  fulladder fulladder1 (ui[3:0], ui[7:4],w, uo_out[4], uo_out[3:0]);
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
+endmodule
+
+module fulladder (  input [3:0] a,
+                  input [3:0] b,
+                  input c_in,
+                  output reg c_out,
+                  output reg [3:0] sum);
+
+	always @ (a or b or c_in) begin
+  	{c_out, sum} = a + b + c_in;
+  end
 endmodule
